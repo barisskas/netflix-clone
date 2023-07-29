ef2070543789f650bf722089f4e9abdec34e8400
barisskas <ecaepraw@gmail.com>
2023-07-29 19:29:02 +0300

    Add auth page

---
+++ src/pages/auth.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+import Input from "@/components/Input";
+const Auth = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState<"login" | "signup">("login");
+
+  const toggleVariant = () => {
+    setVariant((prev) => (prev === "login" ? "signup" : "login"));
+  };
+  return (
+    <div className="relative h-full w-full  bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-ful h-full lg:bg-opacity-50">
+        <nav className=" px-12 py-5">
+          <img src="/images/logo.png" alt="logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-12 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Sign up"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "signup" && (
+                <Input
+                  id="name"
+                  label="Username"
+                  type="text"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                />
+              )}
+              <Input
+                id="email"
+                label="Email"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+              <Input
+                id="password"
+                label="Password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
