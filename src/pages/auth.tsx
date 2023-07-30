92880841374041f84b537c3fda26837a6d50015a
barisskas <ecaepraw@gmail.com>
2023-07-30 15:00:39 +0300

    Updated the authentication flow to support multiple social providers.

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -4,6 +4,10 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 
 import Input from "@/components/Input";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 const Auth = () => {
   const router = useRouter();
   const [name, setName] = useState("");
@@ -85,6 +89,42 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="
+              w-10
+              h-10
+              bg-white
+              rounded-full
+              flex 
+              items-center
+              justify-center
+              cursor-pointer
+              hover:opacity-80
+              transition
+              "
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="
+              w-10
+              h-10
+              bg-white
+              rounded-full
+              flex 
+              items-center
+              justify-center
+              cursor-pointer
+              hover:opacity-80
+              transition
+              "
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "Don't have an account?"
